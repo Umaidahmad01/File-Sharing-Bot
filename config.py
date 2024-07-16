5e0e3fcb87e0bc56fd86161e18109e5acc6d0b03
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-07-16 16:51:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,10 +33,10 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cluster0")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "4"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1002112596877")
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002072642438")
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001930406310")
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002181571156")
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1002112596877"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002072642438"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001930406310"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002181571156"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4")
 
